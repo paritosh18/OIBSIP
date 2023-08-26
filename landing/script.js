ff1ac7f567268a92907ee448bd8a0bbc324001b2
paritosh18 <130278409+paritosh18@users.noreply.github.com>
2023-08-26 10:56:48 +0530

    Add files via upload

---
+++ landing/script.js
@@ -0,0 +1,7 @@
+function zoomImage(item) {
+    item.querySelector(".zoom-overlay").style.opacity = "1";
+}
+
+function resetZoom(item) {
+    item.querySelector(".zoom-overlay").style.opacity = "0";
+}
